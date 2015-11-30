f5e78d1a60658962f530aa8d1b7b9b55158c3383
Max Hunter <nippoo@nippoo.net>
2015-11-30 18:36:37 +0000

    Working spike_clusters_to_kwik.py

--- spike_clusters_into_kwik.py
+++ spike_clusters_into_kwik.py
@@ -1,10 +1,21 @@
 import numpy as np
-import phy
+from phy.io import KwikModel
 import sys
 
-def spike_clusters_into_kwik(scname, kwikname):
-	try:
-		sc = np.loadtxt(scname, skiprows=1, dtype=int)
-	except IOError, OSError:
-		print("Error opening file {0} ({1}): {2}".format(scname, e.errno, e.strerror)
-		return
+def spike_clusters_into_kwik(scname, kwikname, clustering_name="interim"):
+    try:
+    	print("Loading {0}".format(scname))
+    	sc = np.loadtxt(scname, dtype=int) - 1
+    	print("Loaded {0} spikes".format(len(sc)))
+    except(IOError, OSError):
+    	print("Error opening file {0} ({1}): {2}".format(scname, e.errno, e.strerror))
+    	exit()
+
+    model = KwikModel(kwikname)
+    print("Adding {0} into {1}, clustering {3}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    model.add_clustering(clustering_name, sc)
+
+if (len(sys.argv) != 4):
+    print("Usage: spike_clusters_into_kwik.py SCNAME KWIKNAME CLUSTERINGNAME")
+else:
+    spike_clusters_into_kwik(sys.argv[1], sys.argv[2], sys.argv[3])
