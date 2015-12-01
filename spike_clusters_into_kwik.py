f9bcafb3d82c5123a2b33839db651f8b0c9fd81b
Max Hunter <nippoo@nippoo.net>
2015-12-01 20:19:54 +0000

    Bugfix

--- spike_clusters_into_kwik.py
+++ spike_clusters_into_kwik.py
@@ -2,7 +2,7 @@ import numpy as np
 from phy.io import KwikModel
 import sys
 
-def spike_clusters_into_kwik(scname, kwikname, clustering_name="interim"):
+def spike_clusters_into_kwik(scname, kwikname, clustering_name):
     try:
     	print("Loading {0}".format(scname))
     	sc = np.loadtxt(scname, dtype=int) - 1
